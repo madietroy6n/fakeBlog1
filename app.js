785ad2e231b415cc33b67d6822b7ce72d0c49216
Madie Troy <madie.troy6n@gmail.com>
2020-10-05 17:42:48 +0000

    updated app.css, made a deystroy route, added some more styling

--- app.js
+++ app.js
@@ -101,6 +101,19 @@ app.put("/blogs/:id", function(req,res){
 	});
 });
 
+// DELETE ROUTE
+app.delete("/blogs/:id", function(req, res){
+	// destroy blog
+	Blog.findByIdAndRemove(req.params.id, function(err){
+		if(err){
+			res.redirect("/blogs");
+		} else {
+			res.redirect("/blogs");
+		}
+	})
+	// redirect somewhere
+}); 
+
 
 app.listen(process.env.PORT || 8000, function() {
   console.log("Your Server Is Running");
