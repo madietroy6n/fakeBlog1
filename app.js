7e65129e5df8b5988e3d6020ed2984b0bf4ab0bc
Madie Troy <madie.troy6n@gmail.com>
2020-10-05 15:50:41 +0000

    updated app.js with Show route, more styling and created a page to show the blog posts

--- app.js
+++ app.js
@@ -66,6 +66,17 @@ app.post("/blogs", function(req, res){
 });
 
 
+// SHOW ROUTE
+app.get("/blogs/:id", function(req, res){
+	Blog.findById(req.params.id, function(err, foundBlog){
+		if(err){
+		   res.redirect("/blogs");
+		   } else {
+				res.render("show", {blog: foundBlog});
+		   }
+	})
+});
+
 
 app.listen(process.env.PORT || 8000, function() {
   console.log("Your Server Is Running");
